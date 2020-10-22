ea92fd9b8a827e929e30e93b8d1a3370d5df985c
Khrishp <38533279+Khrishp@users.noreply.github.com>
2020-10-22 15:52:18 -0500

    Update W06-Sanitization.cpp
    Decided to just copy paste the small amount of code I had

--- W06-Sanitization/W06-Sanitization/W06-Sanitization.cpp
+++ W06-Sanitization/W06-Sanitization/W06-Sanitization.cpp
@@ -3,12 +3,25 @@
  * Members: John Reiley, Benjamin Hansen, Justen Neeley, 
  *     Joseph Sanderson, Garrett Smith
  * 
- * Description: 
+ * Description: Basic SQL select statement generator
  *****************************************************/
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string username = "";
+    string password = "";
+    cout << "Username: "; 
+    cin >> username;
+    cout << "Password: "; 
+    cin >> password; 
+
+    cout << endl;
+    cout << "SELECT authenticate" << endl 
+        << "FROM passwordList" << endl 
+        << "WHERE name=" + username + " and password=" + password;
 }
 
