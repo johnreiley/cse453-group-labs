5a0fa2629cf322457b60c8f87855fa46c187718f
benjaminhansen <benjamminhansen@gmail.com>
2020-10-31 23:01:33 -0500

    Added TODOs

--- W07-Memory-Organization/W07-Memory-Organization/W07-Memory-Organization.cpp
+++ W07-Memory-Organization/W07-Memory-Organization/W07-Memory-Organization.cpp
@@ -5,7 +5,7 @@
  *      Joseph Sanderson, Garrett Smith
  *
  * Description: This program displays the call-stack of
- *      the loaded program at execution. It also demontrates
+ *      the loaded program at execution. It also demonstrates
  *      the ability to use that information to change variables
  *      outside of the scope of main.
  *********************************************************/
@@ -13,20 +13,26 @@
 #include <iomanip>
 #include <string>
 #include <sstream>
-// to disable asserts, uncomment this directive
+// to disable asserts, un-comment this directive
 // this will cause the compiler to not compile asserts
-//#define NDEBUG
+#define NDEBUG
 #include <cassert>
 using namespace std;
 
 void one(long number);
 void two(long number);
-void pass() { cout << "You pass :)\n"; }
-void fail() { cout << "You've failed :(\n"; }
+void pass() 
+{ 
+   cout << "You pass :)\n"; 
+}
+void fail() 
+{ 
+   cout << "You've failed :(\n"; 
+}
 const char * passMessage = ":)"; 
 const char * failMessage = ":(";
 
-// display helping funciton for validation of addresses
+// display helping function for validation of addresses
 void displayAddressExamples();
 
 /**********************************************
@@ -90,7 +96,7 @@ string displayCharArray(const char * p)
 {
    string output;
    for (int i = 0; i < 8; i++)
-       output += string(" ") + (p[i] >= ' ' && p[i] <= 'z' ? p[i] : '.');
+      output += string(" ") + (p[i] >= ' ' && p[i] <= 'z' ? p[i] : '.');
    return output;
 }
 
@@ -121,24 +127,24 @@ void two(long number)              // 345678
 
    // header for our table. Use these setw() offsets in your table
    cout << '[' << setw(2) << 'i' << ']'
-        << setw(15) << "address"
-        << setw(20) << "hexadecimal"
-        << setw(20) << "decimal"
-        << setw(18) << "characters"
-        << endl;
+      << setw(15) << "address"
+      << setw(20) << "hexadecimal"
+      << setw(20) << "decimal"
+      << setw(18) << "characters"
+      << endl;
    cout << "----+"
-        << "---------------+"
-        << "-------------------+"
-        << "-------------------+"
-        << "-----------------+\n";
+      << "---------------+"
+      << "-------------------+"
+      << "-------------------+"
+      << "-----------------+\n";
 
    // TODO: define current here
-   for (long i = 34; i >= -4; i--)   // You may need to change 24 to another number
+   char * current;
+   for (long i = 50; i >= -4; i--) 
    {
       ////////////////////////////////////////////////
       // Insert code here to display the callstack
-      // TODO: this should work as well: current += 8; // increment up 8 bytes
-      char* current = (char*)((char*)&bow + 8 * i);
+      current = (char*)((char*)&bow + 8 * i);
 
       cout << '[' << setw(2) << i << ']'
          << setw(15) << (void*)current
@@ -154,19 +160,20 @@ void two(long number)              // 345678
 
    ////////////////////////////////////////////////
    // Insert code here to change the variables in main()
-                                                                                
+
    // change text in main() to "*main**"
 
-    const char * search = (char *)&bow; // start here like assignment says
+   const char * search = (char *)&bow; // start here like assignment says
    // probe the memory until the text in main is found
-   while (string(search) != "*MAIN**") // convert to string for efficient comparison
+   // convert to string for efficient comparison
+   while (string(search) != "*MAIN**")
    {
-       search++; // increment one byte up the stack at a time
+      search++; // increment one byte up the stack at a time
    }
    cout << "\nTrying to change memory here. Found the text!\n"
-        << "\nStarting address...\n"
-        << "Stack: " << &search 
-        << endl;
+      << "\nStarting address...\n"
+      << "Stack: " << (void *)search 
+      << endl;
    cout << "Text was: " << string(search) << endl;
    // not constant so we can change the value
    // get the starting address that we want from the const pointer 
@@ -182,23 +189,26 @@ void two(long number)              // 345678
    changeText[5] = '*';
    changeText[6] = '*'; // change the variable outside of its scope!
 
-   // don't access through changeText... prove we updated what search pointed to
+   // don't access through changeText... 
+   // prove we updated what search pointed to
    cout << "Text now is: " << string(search) << endl;
    search -= 8; // decrement down 8 bytes
    cout << "What is this: " << *((long *)search) << endl;
+
+   cout << "\n\nWHAT'S THE DIFFERENCE!!!\n"  
+      << "main: " << main << endl << "&main: " << &main << endl << endl;
    // we should be at the beginning of the long int in main
    // but we're not.............
 
    // when probing downward, make sure we don't try to decrement past NULL
 
    // change number in main() to 654321
-   
+
    // do it the sure-fire way
    search = (char *)&bow;
    while (*((long *)search) != 123456)
    {
-       cout << "Searching up from bow...\n";
-       search++; // increment down one byte at a time
+      search++; // increment down one byte at a time
    }
    assert(*((long *)search) == 123456);
 
@@ -211,92 +221,111 @@ void two(long number)              // 345678
    // Garrett: since the previous search succeeded, THIS LOOP WON'T RUN
    while (( search != NULL ) && ( *((long *)search) != 123456 ))
    {
-       cout << "Searching down...\n";
-       search--; // increment down one byte at a time
+      cout << "Searching down...\n";
+      search--; // increment down one byte at a time
    }
    if (search == NULL)
    {
-       cout << "Failed search for long int";
+      cout << "Failed search for long int";
    }
    else
    {
-       // we can still change number here
-       cout << "\nTrying to change memory here. Found the long int!\n"
-           << "\nStarting address...\n"
-           << "Stack: " << &search 
-           << endl;
-       cout << "Long number was: " << *((long*)search) << endl;
-       // not constant so we can change the value
-       long * changeNumber = (long *)search; // point to longs
-       *changeNumber = 654321; // change the variable outside of its scope!
-       cout << "Now is: " << *((long*)search) << endl;
+      // we can still change number here
+      cout << "\nTrying to change memory here. Found the long int!\n"
+         << "\nStarting address...\n"
+         << "Stack: " << search 
+         << endl;
+      cout << "Long number was: " << *((long*)search) << endl;
+      // not constant so we can change the value
+      long * changeNumber = (long *)search; // point to longs
+      *changeNumber = 654321; // change the variable outside of its scope!
+      cout << "Now is: " << *((long*)search) << endl;
    }
 
+
    // change pointerFunction in main() to point to pass
-   
+
    // so far I can't get this logic to compile
    // start the search upward (increment search) instead of downward
-   // start the search at (char *)&bow;
+   // start the search at
    // assert assumptions
-   /*
+
    void (*pFunc)() = fail;
    const char * pFuncTest = (char *)pFunc;
+   cerr << "Fail is: " << fail << endl;
    cout << "Testing pFunc: ";
    (*pFunc)();
    cout << "Testing pFuncTest: ";
-   (*pFuncTest)();
-   assert(pFuncTest == fail);
-   assert(pFuncTest == pFunc);
+   // cast it to a void pointer so the compiler will recognize
+   // we're defining a function pointer
+   assert((void *)pFuncTest == fail);
+   assert((void *)pFuncTest == pFunc);
 
    // search down from last location of search (at the address for number) 
-   while (( search != NULL ) && ( search != fail ))
+   // do it the sure-fire way
+   search = (char *)&bow;
+   cout << "Char pointer dereference! " << *(long *)search << endl;
+   long failAddress = (long)fail;
+   while (*(long*)search != failAddress)
    {
-       search--; // increment down one byte at a time
+      search++; // increment up one byte at a time
    }
+   assert((long)fail == *(long *)search);
    cout << "\nTrying to change memory here. Found the pointerFunction!\n"
-        << "\nStarting address...\n"
-        << "Stack: " << &search 
-        << endl;
-   cout << "pointerFunction number was: ";
-   (*search)();
-   cout << endl;
-   // not constant so we can change the value
-   void (*changePointerFunction) = (void *)search; // point to longs
-   changePointerFunction = pass; // change the variable outside of its scope!
-   cout << "Now is: ";
-   (*search)();
-   cout << endl;
-   */
-   
+      << "\nStarting address...\n"
+      << "Stack: " << (void *)search
+      << "\nBefore: " << *(long *)search;
    // change message in main() to point to passMessage
-
+   search = (char *)&bow;
+   char ** messageSearch = (char **)&bow;
+   while (string(*messageSearch) != string(failMessage))
+   {
+      cout << "Searching up ...\n";
+      messageSearch++; // increment down one byte at a time
+   }
+   cout << "\nTrying to change memory here. Found the message!\n"
+      << "\nStarting address...\n"
+      << "Stack: " << (void *)search 
+      << endl;
+   cout << "Message was: " << string(search) << endl;
+   // not constant so we can change the value
+   // set the changeMessage pointer to point to the message pointer in main
+   // accessed by search
+   const char ** changeMessage = (const char **)search; // point to longs
+   *changeMessage = passMessage; // change the variable outside of its scope!
+   cout << "Now is: " << string(search) << endl;
+   assert(string(*changeMessage) == ":)");
+   assert(string(search) == ":)");
+   assert(string(failMessage) == ":(");
+   assert(string(failMessage) != string(search));
+   assert(string(failMessage) != string(*changeMessage));
    //
    ////////////////////////////////////////////////
 }
 
-/******************************************************************************
+/**************************************************************************
  * displayAddressExamples
  * Not needed, but useful for seeing what the address displays look like for
  * stack variable, heap memory, or code segment.
  * ANY AND ALL CAN EDIT THIS CODE AS THEY PLEASE, within reason
- ******************************************************************************/
+ **************************************************************************/
 void displayAddressExamples()
 {
-    cout << "\nDisplaying address examples\n";
-    int funNum = 99;
+   cout << "\nDisplaying address examples\n";
+   int funNum = 99;
 
-    // Stack variables
-    cout << "Stack: " << &funNum << endl;
-    
-    int * pHeap = new int(5); // initialized to 5
-    // Heap address
-    cout << "Heap: " << pHeap << endl;
+   // Stack variables
+   cout << "Stack: " << &funNum << endl;
 
-    static string addressed = "this is here";
-    // Code segment
-    cout << "Code: " << &addressed<< endl;
+   int * pHeap = new int(5); // initialized to 5
+   // Heap address
+   cout << "Heap: " << pHeap << endl;
 
-    // don't forget to free
-    delete pHeap;
-    pHeap = NULL;
+   static string addressed = "this is here";
+   // Code segment
+   cout << "Code: " << &addressed << endl;
+
+   // don't forget to free
+   delete pHeap;
+   pHeap = NULL;
 }
