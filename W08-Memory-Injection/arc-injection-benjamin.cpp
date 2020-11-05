37ce9086483baf7e8ae61cac9c32db22f833e01e
benjaminhansen <benjamminhansen@gmail.com>
2020-11-04 21:21:53 -0600

    created arc vulnerability function

--- W08-Memory-Injection/arc-injection-benjamin.cpp
+++ W08-Memory-Injection/arc-injection-benjamin.cpp
@@ -10,9 +10,39 @@
  *      vulnerability."
  * 3.  "After the memory is overwritten, the function 
  *      pointer must be dereferenced."
+ * The solution was helped by Baltasarq's post in this 
+ * Stack Overflow post:
+ * https://stackoverflow.com/questions/3811328/try-to-write-char-to-a-text-file
  **********************************************************/
-void arcVulnerability(/* arguments */)
+void arcVulnerability(const string & key)
 {
+   // load the 128-byte key into a char buffer
+   char buffer[128];
+   void (* endMessage)() = saved;
+
+   // write the characters to buffer
+   for (int i = 0; i <= 128; ++i)
+   {
+      buffer[i] = key[i];
+   }
+
+   // write the key to a character text file
+   ofstream ofs; // solely for outputting to the file
+   ofs.open("special-key.txt", ios::text);
+   // check for failures
+   if(ofs.fail())
+   {
+      endMessage = failedSave;
+   }
+   else
+   {
+      ofs.write(buffer, 128);
+      ofs.close(); // close the file stream
+   }
+
+   // display end message
+   (*endMessage)();
+
    return;
 }
 
@@ -27,7 +57,8 @@ void arcVulnerability(/* arguments */)
  **********************************************************/
 void arcExploit()
 {
-   arcVulnerability(/* parameters with malicious data */);
+   string key = "cUWt+]}IBr/b~d ;~b&yW<C|<f Klu%3d[WUZ;P*E<lr=~mxJnIP}bf#a#<+.kZN.[}RT_<8vp>B*] ;.5Kn~p+e$NFZVN+j9sL&8(P:pZ/nH|MW/ECr.?sz@fIKJ$`.";
+   arcVulnerability(key);
    return;
 }
 
@@ -37,6 +68,7 @@ void arcExploit()
  **********************************************************/
 void arcWorking()
 {
-   arcVulerability(/* parameters with no malicious data */);
+   string key = "cUWt+]}IBr/b~d ;~b&yW<C|<f Klu%3d[WUZ;P*E<lr=~mxJnIP}bf#a#<+.kZN.[}RT_<8vp>B*] ;.5Kn~p+e$NFZVN+j9sL&8(P:pZ/nH|MW/ECr.?sz@fIKJ$`.";
+   arcVulnerability(key);
    return;
 }
