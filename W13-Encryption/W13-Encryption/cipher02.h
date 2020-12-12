87f80f3c04e758ccf584d8fa6248af364b1e0a6b
Justen-N <losthalflingrogue@gmail.com>
2020-12-12 14:08:28 -0700

    Finally, Cipher02 encrypt is done.

--- W13-Encryption/W13-Encryption/cipher02.h
+++ W13-Encryption/W13-Encryption/cipher02.h
@@ -1,3 +1,4 @@
+#include <list>
 /********************************************************************
 * Header:
 *    Implement your cipher here. You can view 'example.h' to see the
@@ -44,15 +45,109 @@ public:
       return str;
    }
 
+   void buildPolybiusSquare(char square[][5], std::string key)
+   {
+	   std::string alphabet = "abcdefghi*klmnopqrstuvwxyz";
+	   std::string polybius = "";
+	   for (int i = 0; i < key.size(); i++)
+	   {
+		   if (key[i] != 'j')
+		   {
+
+			   key[i] = tolower(key[i]);
+		   }
+		   else
+		   {
+			   continue;
+		   }
+
+		   if (key[i] == alphabet[(int)key[i] - 97])
+		   {
+			   polybius += key[i];
+			   alphabet[(int)key[i] - 97] = '*';
+
+		   }
+
+	   }
+	   polybius += alphabet;
+	   int i = 0;
+	   for (int r = 0; r < 5; r++)
+		   for (int c = 0; c < 5; c++)
+		   {
+
+			   square[c][r] = polybius[i];
+			   i++;
+			   while (polybius[i] == '*')
+				   i++;
+		   }
+   }
+
+
+   void displayPolybiusSquare(char alphabet[][5])
+   {
+	   for (int r = 0; r < 5; r++)
+	   {
+		   for (int c = 0; c < 5; c++)
+		   {
+			   std::cout << " " << alphabet[c][r] << " ";
+		   }
+		   std::cout << std::endl;
+	   }
+   }
+
+   void find(char alphabet[][5], char findme, int& x, int& y)
+   {
+	   for (int r = 0; r < 5; r++)
+	   {
+		   for (int c = 0; c < 5; c++)
+		   {
+			   if (alphabet[c][r] == findme)
+			   {
+				   x = c;
+				   y = r;
+				   return;
+			   }
+
+		   }
+	   }
+   }
    /**********************************************************
     * ENCRYPT
     * TODO: ADD description
     **********************************************************/
    virtual std::string encrypt(const std::string& plainText,
-      const std::string& password)
+	   const std::string& password)
    {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
+		  std::string temp = plainText;
+		  std::string cipherText = "";
+		  char alphabet[5][5];
+		  buildPolybiusSquare(alphabet, password);
+		  displayPolybiusSquare(alphabet);
+		  int x, y;
+		  std::list<int> xs;
+		  std::list<int> ys;
+		  for (int i = 0; i < temp.size(); i++)
+		  {
+
+			  if (temp[i] == 'j')
+			  {
+				  temp[i] = 'i';
+			  }
+			  if (temp[i] == ' ')
+			  {
+				  continue;
+			  }
+			  find(alphabet, temp[i], x, y);
+			  xs.push_back(x);
+			  ys.push_back(y);
+		  }
+		  xs.splice(xs.end(), ys);
+		  for (auto it = xs.begin(); it != xs.end();advance(it,2)) 
+		  {
+
+			  cipherText += alphabet[*it][*std::next(it)];
+		  }
+		  
       return cipherText;
    }
 
